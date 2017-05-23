653ef4ad97082399572b65ca253b0ba546a3b48f
rachelbottino <rachelpbm@gmail.com>
2017-05-23 15:49:05 -0300

    mapa funcionando + inicio servlet

--- server.py
+++ server.py
@@ -1,22 +1,44 @@
-from flask import Flask, render_template
-from flask.ext.socketio import SocketIO, emit
+from flask import Flask, render_template, request
+# from flask.ext.socketio import SocketIO, emit
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+# socketio = SocketIO(app)
 
-values = {
-    'slider1': 25,
-    'slider2': 0,
-}
+dic_farm = {'rachelpbm@gmail.com': 123456, 'matheus_dmd@gmail.com': 123456, 'gabi_almeida@gail.com' : 123456}
+
+def __init__(self, email, password):
+        self.email = email
+        self.password = password
 
 @app.route('/')
 def index():
-    return render_template('index.html', **values)
+	return render_template('smartfarm_home.html')
 
-@socketio.on('value changed')
-def value_changed(message):
-    values[message['who']] = message['data']
-    emit('update value', message, broadcast=True)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if request.method == 'GET':
+		print("tentativa login")
+		# email = request.form["email"]
+		# password = request.form["pwd"]
+		# print(email)
+		# print(password)
+		
+		# if dic_farm[email] == password:
+		# 	print('sucesso no login')
+		return render_template('smartfarm_user.html')
+
+		# else:
+		# 	print('login negado')
+		# 	return render_template('smartfarm_home.html')
+	else:
+		print("olé")
+		return render_template('smartfarm_home.html')
+
+# @socketio.on('value changed')
+# def value_changed(message):
+#     values[message['who']] = message['data']
+#     emit('update value', message, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0')
+	app.run(host='0.0.0.0')
+#    socketio.run(app, host='0.0.0.0')
